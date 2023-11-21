f1923be3c5bcb065767948e9b317d6b1f063405a
perpyderp <g4m3rall3n@gmail.com>
2023-11-20 21:47:37 -0500

    Installed axios. Added button to registration page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -17,6 +17,10 @@ import {
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
 
+import axios from "axios";
+import { redirect } from "next/dist/server/api-utils"
+import Error from "next/error"
+
 const formSchema = z.object({
     username: z.string().min(2).max(50),
     email: z
@@ -44,7 +48,17 @@ export default function Register() {
     // 2. Define a submit handler.
     function onSubmit(values: z.infer<typeof formSchema>) {
         // Do something with the form values.
-        // ✅ This will be type-safe and validated.
+        axios.post("http://localhost:8080/api/auth/register", {
+            body: form
+        })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error: Error) => {
+                console.log(error);
+            })
+            
+
         console.log(values)
     }
 
@@ -52,48 +66,51 @@ export default function Register() {
         <>
             
         <Form {...form}>
-            <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>This is your public display name.</FormDescription>
-                    <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>For verification and recovery</FormDescription>
-                    <FormMessage />
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                    <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>For verification and recovery</FormDescription>
-                    <FormMessage />
-                    </FormItem>
-                )}
-            />
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>This is your public display name.</FormDescription>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>For verification and recovery</FormDescription>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>Enter a secure password</FormDescription>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
         </Form>
 
             
