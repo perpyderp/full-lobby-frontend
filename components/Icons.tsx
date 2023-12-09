bd9238452d460bc76da8e887d5091ac6aafb02aa
perpyderp <g4m3rall3n@gmail.com>
2023-12-09 17:29:31 -0500

    Added user icon, creat user avatar component

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,6 +1,7 @@
-import { LucideProps } from "lucide-react";
+import { LucideProps, User } from "lucide-react";
 
 export const Icons = {
+    user: User,
     logo: (props: LucideProps) => (
         <svg
             xmlns="http://www.w3.org/2000/svg"
