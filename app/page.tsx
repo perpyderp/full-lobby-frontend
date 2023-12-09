d08c82614956d58d476be8a88296e5d72a2f0c58
perpyderp <g4m3rall3n@gmail.com>
2023-12-08 22:31:52 -0500

    Added basic content to home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,25 @@ export default function Home() {
 
   return (
     <>
-      Home Page
+      <div className="grid grid-cols-4">
+        <div>
+          <h3>Username</h3>
+          <h4>Daily Tasks</h4>
+          <ul>
+            <li>Create a post</li>
+            <li>Like 5 posts</li>
+            <li>Comment on a post</li>
+          </ul>
+          <h4>Weekly Tasks</h4>
+          <ul>
+            <li>Like 40 posts</li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        Posts feed
+      </div>
     </>
   )
 }
