988b37660bd0cfa514d89d152647785e287075fe
perpyderp <g4m3rall3n@gmail.com>
2023-12-12 18:50:08 -0500

    Fixed apostrophe in Error page

--- app/profile/[username]/error.tsx
+++ app/profile/[username]/error.tsx
@@ -5,7 +5,7 @@ export default function ProfileError() {
 
     return (
         <div className="flex flex-col justify-center items-center p-20 text-center">
-            <p className="">Sorry! The user you're looking for could not be found</p>
+            <p className="">Sorry! The user you&apos;re looking for could not be found</p>
         </div>
     )
 }
